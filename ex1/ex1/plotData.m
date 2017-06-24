d444d8bcd26340a5e0392877b6cd15c1ab54c0bb
nick serebrennikov <non.manifold@gmail.com>
2017-06-24 10:31:43 +0100

    plot and cost function for linear regression

--- ex1/ex1/plotData.m
+++ ex1/ex1/plotData.m
@@ -18,7 +18,9 @@ figure; % open a new figure window
 
 
 
-
+plot(x,y, 'rx', 'MarkerSize', 10);
+xlabel('population');
+ylabel('revenue');
 
 % ============================================================
 
