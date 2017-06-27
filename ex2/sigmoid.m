3ff1f0403507c0ae5b5bb9fc3928e5b78e25cf67
nick serebrennikov <non.manifold@gmail.com>
2017-06-27 20:27:17 +0100

    correct sigmoid implementation with arrayfun

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -3,16 +3,12 @@ function g = sigmoid(z)
 %   g = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-exponents=exp(z.*-1);
-for i=1 : size(z)
-  g(i)= 1 / (1 + exponents(i));
-end
+g= arrayfun(@(x) 1/(1+e^(-x)), z);
 
 % =============================================================
 
