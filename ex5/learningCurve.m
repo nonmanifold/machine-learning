dee5bd0fb7150e4178fb44b1e486a7b4994c8340
nick serebrennikov <Nick.Serebrennikov@derivco.co.uk>
2017-07-22 21:24:25 +0100

    Learning Curve

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -53,11 +53,15 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+  xi=X(1:i, :);
+  yi=y(1:i);
+  
+  [theta] = trainLinearReg(xi, yi, lambda);
+  [error_train(i)]= linearRegCostFunction(xi, yi, theta, 0);
+  [error_val(i)]=linearRegCostFunction(Xval, yval, theta, 0);
 
-
-
-
-
+end
 
 % -------------------------------------------------------------
 
