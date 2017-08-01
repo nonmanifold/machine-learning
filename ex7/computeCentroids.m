df9d022fd45eb4fdcf93d14fa4622d4869e07d9a
nick serebrennikov <Nick.Serebrennikov@derivco.co.uk>
2017-08-01 21:16:27 +0100

    Compute Centroid Means (k-Means)

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -26,8 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
+for k=1:K
+  Ck = X(idx==k, :);
+  centroids(k, :)=(1/size(Ck,1))*sum(Ck);
+end
 
 
 
