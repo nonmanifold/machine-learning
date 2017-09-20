1485571e1d5ff2224c88e39badcdeb277123e213
nick serebrennikov <Nick.Serebrennikov@derivco.co.uk>
2017-09-20 20:31:06 +0100

    data

--- ex8-recomend/src-prices.sql
+++ ex8-recomend/src-prices.sql
@@ -1,5 +1,6 @@
 SELECT p.id, SUM(p.price) AS income, c.[user] FROM products AS p, carts AS c, cartItems AS ci
 WHERE p.id = ci.product
 	AND c.id = ci.cart
+	AND p.price IS NOT NULL
 GROUP BY p.id, c.[user]
 ORDER BY income DESC;
